2d7b4b1f3b0a91c08bae85152aa62090b9346b7f
Antarctic <wcgui@yahoo.com>
2017-09-12 15:57:27 +0800

    9.12 wcg nav  要提交的变更： 	修改：     components/index.js 	新文件：   public/stylesheets/foot_qz.css 	新文件：   public/stylesheets/nav.css 	修改：     views/foot_qz.ejs 	修改：     views/head.ejs 	修改：     views/index.ejs

--- components/index.js
+++ components/index.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Head from './views/head';
-import Foot from './views/foot';
 import { Router, Route} from 'react-router';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 class Index extends React.Component {
+
   render(){
     return (
-      <div>
-        <h1>Hello {window.ejsname} </h1>
-        <br />
-
-        <h3>{userFromServer.name}</h3>
-        <h3>{userFromServer.user}</h3>
-        <h3>{userFromServer.sex}</h3>
-        <h3>{userFromServer.age}</h3>
-        <Head name="React">
-        </Head>
-        <hr></hr>
-        <Foot></Foot>
-      </div>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">思习培训班</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#one">首页</NavItem>
+            <NavItem eventKey={2} href="#three">在线报名</NavItem>
+            <NavDropdown eventKey={3} title="已报课程" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">登录</NavItem>
+            <NavItem eventKey={2} href="#">注册</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
-  }
+};
+
 }
 
 
 ReactDOM.render(
   <Index />,
-  document.getElementById('react-root')
+  document.getElementById('root')
 );
